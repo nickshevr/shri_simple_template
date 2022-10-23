30c54250a809db99df80fc1741cbd89e58d196bd
JeeEssEm <67123971+JeeEssEm@users.noreply.github.com>
2022-10-23 22:44:44 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["airbnb", "eslint:recommended", "plugin:react/recommended"],
+  extends: ["eslint:recommended", "plugin:react/recommended"],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
