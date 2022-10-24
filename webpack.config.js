2d17e9745b171846161e69c207fba099a11e460e
Haping61 <tg.trippl3a@gmail.com>
2022-10-24 21:48:41 +0700

    lint clear code

--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,34 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const StatoscopePlugin = require('@statoscope/webpack-plugin').default;
 
 const config = {
-    entry: {
-        about: './src/pages/About.js',
-        home: './src/pages/Home.js',
-    },
-    plugins: [
-        new HtmlWebpackPlugin(),
-        new StatoscopePlugin({
-            saveStatsTo: 'stats.json',
-            saveOnlyStats: false,
-            open: false,
-        }),
+  entry: {
+    about: './src/pages/About.js',
+    home: './src/pages/Home.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new StatoscopePlugin({
+      saveStatsTo: 'stats.json',
+      saveOnlyStats: false,
+      open: false,
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      // @TODO js rule
+      // @TODO css rule
     ],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js',
-    },
-    module: {
-        rules: [
-            // @TODO js rule
-            // @TODO css rule
-        ],
-    },
-    // @TODO optimizations
-    // @TODO lodash treeshaking
-    // @TODO chunk for lodash
-    // @TODO chunk for runtime
-    // @TODO fallback for crypto
+  },
+  // @TODO optimizations
+  // @TODO lodash treeshaking
+  // @TODO chunk for lodash
+  // @TODO chunk for runtime
+  // @TODO fallback for crypto
 };
 
 module.exports = config;
