1a0c3876cbbd92e3264f710ce19b1ce4b57cd1c6
German Ivanov <germanivanov0719@gmail.com>
2022-10-27 22:58:50 +0300

    Initial commit, webpack and linting works

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,56 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const StatoscopePlugin = require('@statoscope/webpack-plugin').default;
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const StatoscopePlugin = require("@statoscope/webpack-plugin").default;
 
 const config = {
-    entry: {
-        about: './src/pages/About.js',
-        home: './src/pages/Home.js',
-    },
-    plugins: [
-        new HtmlWebpackPlugin(),
-        new StatoscopePlugin({
-            saveStatsTo: 'stats.json',
-            saveOnlyStats: false,
-            open: false,
-        }),
+  entry: {
+    about: "./src/pages/About.js",
+    home: "./src/pages/Home.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new StatoscopePlugin({
+      saveStatsTo: "stats.json",
+      saveOnlyStats: false,
+      open: false,
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(html|htm)$/i,
+        loader: "html-loader",
+        exclude: ["/node_modules/"],
+      },
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: "ts-loader",
+        exclude: ["/node_modules/"],
+      },
+      {
+        test: /\.(js|jsx)$/i,
+        loader: "babel-loader",
+        exclude: ["/node_modules/"],
+      },
+      {
+        test: /\.(css|css)$/i,
+        loader: "css-loader",
+        exclude: ["/node_modules/"],
+      },
     ],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js',
-    },
-    module: {
-        rules: [
-            // @TODO js rule
-            // @TODO css rule
-        ],
-    },
-    // @TODO optimizations
-    // @TODO lodash treeshaking
-    // @TODO chunk for lodash
-    // @TODO chunk for runtime
-    // @TODO fallback for crypto
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx", ".js", ".css"],
+  },
+  // @TODO optimizations
+  // @TODO lodash treeshaking
+  // @TODO chunk for lodash
+  // @TODO chunk for runtime
+  // @TODO fallback for crypto
 };
 
 module.exports = config;
