202ffb8d368b5e577e6d05d940a95e2d12db6141
Salavat <salavatmerhatimovic@gmail.com>
2022-10-27 21:20:21 +0300

    fix:удалил не используемое в вебконфиге

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,44 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { allowedNodeEnvironmentFlags } = require("process");
-const StatoscopePlugin = require("@statoscope/webpack-plugin").default;
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const StatoscopePlugin = require('@statoscope/webpack-plugin').default;
 
 const config = {
   entry: {
-    index: "./src/index.js",
-    about: "./src/pages/About.js",
-    home: "./src/pages/Home.js",
+    index: './src/index.js',
+    about: './src/pages/About.js',
+    home: './src/pages/Home.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html',
     }),
     new StatoscopePlugin({
-      saveStatsTo: "stats.json",
-      saveReportTo: "report.html",
+      saveStatsTo: 'stats.json',
+      saveReportTo: 'report.html',
       saveOnlyStats: false,
       open: false,
     }),
   ],
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
   },
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, 'dist'),
   },
-  mode: "development",
-  //target: "web",
+  mode: 'development',
+  // target: "web",
   module: {
     rules: [
       // js rule
       {
         test: /\.(js|jsx)$/,
         exclude: [/node_modules/],
-        loader: "babel-loader",
+        loader: 'babel-loader',
         options: {
           presets: [
-            "@babel/preset-env",
-            ["@babel/preset-react", { runtime: "automatic" }],
+            '@babel/preset-env',
+            ['@babel/preset-react', { runtime: 'automatic' }],
           ],
         },
       },
@@ -48,18 +46,18 @@ const config = {
       {
         test: /\.css$/,
         exclude: [/node_modules/],
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(html)$/,
-        loader: "html-loader",
+        loader: 'html-loader',
       },
     ],
   },
   // fallback for stream
   resolve: {
     fallback: {
-      stream: require.resolve("stream-browserify"),
+      stream: require.resolve('stream-browserify'),
     },
   },
 
@@ -70,10 +68,10 @@ const config = {
     usedExports: true,
     innerGraph: true,
 
-    moduleIds: "deterministic",
-    runtimeChunk: "single",
+    moduleIds: 'deterministic',
+    runtimeChunk: 'single',
     splitChunks: {
-      chunks: "all",
+      chunks: 'all',
     },
   },
 };
