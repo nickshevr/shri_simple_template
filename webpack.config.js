ceecb2eed0a0a67bf50297ace87549e25bc9d0ca
German Ivanov <germanivanov0719@gmail.com>
2022-10-27 23:10:56 +0300

    Fix "crypto" module problem
    A related error was logged in browser console every time, fixed by commenting out "crypto" import, as browsers already provide it these days

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,50 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const StatoscopePlugin = require("@statoscope/webpack-plugin").default;
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const StatoscopePlugin = require('@statoscope/webpack-plugin').default;
 
 const config = {
   entry: {
-    about: "./src/pages/About.js",
-    home: "./src/pages/Home.js",
+    about: './src/pages/About.js',
+    home: './src/pages/Home.js',
   },
   plugins: [
     new HtmlWebpackPlugin(),
     new StatoscopePlugin({
-      saveStatsTo: "stats.json",
+      saveStatsTo: 'stats.json',
       saveOnlyStats: false,
       open: false,
     }),
   ],
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
   },
   module: {
     rules: [
       {
         test: /\.(html|htm)$/i,
-        loader: "html-loader",
-        exclude: ["/node_modules/"],
+        loader: 'html-loader',
+        exclude: ['/node_modules/'],
       },
       {
         test: /\.(ts|tsx)$/i,
-        loader: "ts-loader",
-        exclude: ["/node_modules/"],
+        loader: 'ts-loader',
+        exclude: ['/node_modules/'],
       },
       {
         test: /\.(js|jsx)$/i,
-        loader: "babel-loader",
-        exclude: ["/node_modules/"],
+        loader: 'babel-loader',
+        exclude: ['/node_modules/'],
       },
       {
         test: /\.(css|css)$/i,
-        loader: "css-loader",
-        exclude: ["/node_modules/"],
+        loader: 'css-loader',
+        exclude: ['/node_modules/'],
       },
     ],
   },
   resolve: {
-    extensions: [".js", ".ts", ".tsx", ".js", ".css"],
+    extensions: ['.js', '.ts', '.tsx', '.js', '.css'],
   },
   // @TODO optimizations
   // @TODO lodash treeshaking
