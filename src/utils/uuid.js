30c02964579d247f47af2a5e6e7d8cb18ec854f6
Ru1ka <wotb5ks@gmail.com>
2022-10-28 15:38:35 +0300

    compilation successfull with bugs

--- src/utils/uuid.js
+++ src/utils/uuid.js
@@ -1,4 +1,9 @@
-import crypto from 'crypto-browserify';
+// import crypto from 'crypto-browserify';
 
-// генерируем какой-то идентификатор для запуска
-export const UUID = crypto.randomUUID();
+// // генерируем какой-то идентификатор для запуска
+// export const UUID = crypto.randomUUID();
+
+
+import { v4 as uuidv4 } from 'uuid';
+
+export const UUID = uuidv4;
--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,22 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const StatoscopePlugin = require('@statoscope/webpack-plugin').default;
 
+
 const config = {
     entry: {
+        index: './src/index.js',
         about: './src/pages/About.js',
         home: './src/pages/Home.js',
     },
     plugins: [
-        new HtmlWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: "React App",
+            template: './public/index.html'            
+        }),
         new StatoscopePlugin({
-            saveStatsTo: 'stats.json',
+            name: "shri_simple_template",
+            saveReportTo: 'report-[name]-[hash].html',
+            saveStatsTo: 'stats-[name]-[hash].json',
             saveOnlyStats: false,
             open: false,
         }),
@@ -22,13 +29,34 @@ const config = {
     module: {
         rules: [
             // @TODO js rule
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                options: {
+                    "presets": [
+                        "@babel/preset-env",
+                        ["@babel/preset-react", {"runtime": "automatic"}]]
+                    }
+            },
             // @TODO css rule
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
         ],
     },
+
     // @TODO optimizations
+    optimization: {
+        usedExports: true,
+        runtimeChunk: {name: 'runtime'}
+    },
     // @TODO lodash treeshaking
+    // Done 👆
     // @TODO chunk for lodash
     // @TODO chunk for runtime
+    // Done 👆
     // @TODO fallback for crypto
 };
 
