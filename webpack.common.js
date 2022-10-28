05ea5d531285598b4f50a8871399d38c83244b44
Vladislav <gubarev20084848@gmail.com>
2022-10-28 15:08:26 +0300

    tf is going on

--- webpack.common.js
+++ webpack.common.js
@@ -1,82 +1,7 @@
 
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
-// const webpack = require("webpack");
-// const path = require("path");
-// const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const StatoscopePlugin = require("@statoscope/webpack-plugin").default;
  const InterpolateHtmlPlugin = require("interpolate-html-plugin");
-// const dotenv = require("dotenv");
-// dotenv.config();
-// const config = {
-//   entry: {
-//     about: "./src/pages/About.js",
-//     home: "./src/pages/Home.js",
-//   },
 
-//   output: {
-//     path: path.resolve(__dirname, "dist"),
-//     filename: "[name].[contenthash].js",
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(js|jsx)$/,
-//         exclude: ["/node_modules/"],
-//         use: {
-//           loader: "babel-loader",
-//         },
-//       },
-//       {
-//         test: /\.html$/,
-//         use: {
-//           loader: "html-loader",
-//         },
-//       },
-//       {
-//         test: /\.css$/i,
-//         use: ["style-loader", "css-loader"],
-//       },
-//     ],
-//   },
-//   resolve: {
-//     extensions: [".js", ".jsx", ".css"],
-//     // fallback: {
-//     //   stream: require.resolve("stream-browserify"),
-//     //   crypto: require.resolve("crypto-browserify"),
-//     // },
-//   },
-//   plugins: [
-//     new NodePolyfillPlugin(),
-//     new webpack.ProvidePlugin({
-//       process: "process/browser",
-//     }),
-//     new webpack.DefinePlugin({
-//       "process.env": JSON.stringify(process.env),
-//     }),
-//     new HtmlWebpackPlugin({
-//       filename: "index.html",
-//       inject: true,
-//       template: "public/index.html",
-//     }),
-//     new InterpolateHtmlPlugin({
-//       PUBLIC_URL: "public",
-//     }),
-//     new StatoscopePlugin({
-//       saveStatsTo: "stats.json",
-//       saveOnlyStats: false,
-//       open: false,
-//     }),
-//   ],
-
-//   // @TODO optimizations
-//   // @TODO lodash treeshaking
-//   // @TODO chunk for lodash
-//   // @TODO chunk for runtime
-//   // @TODO fallback for crypto
-// };
-
-// module.exports = config;
 
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
@@ -113,14 +38,17 @@ const config = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        resolve: {
-          extensions: [".js", ".jsx"],
-        },
+        test: /\.(js|jsx)$/i,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              ['@babel/preset-react', { runtime: 'automatic' }]],
+          },
         },
+        exclude: /(node_modules)/,
+        resolve: { extensions: ['.js', '.jsx'] },
       },
       {
         test: /\.(s[ac]|c)ss$/i, // /\.(le|c)ss$/i если вы используете less
@@ -135,12 +63,11 @@ const config = {
   target: "web",
   resolve: {
     fallback: {
-      crypto: "crypto-browserify",
-      stream: "stream-browserify",
+      'crypto': require.resolve('crypto'),
     },
     alias: {
-      crypto:"crypto-browserify",
-      stream: "stream-browserify",
+      'crypto-browserify': path.resolve(__dirname, 'src/crypto-fallback.js'),
+      'react-is': path.resolve(__dirname, 'node_modules/react-is/cjs/react-is.production.min.js')
     },
   },
   optimization: {},
