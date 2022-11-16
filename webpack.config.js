d532452ad9e52ba46c8c34164bb631e2ae7155c6
TimurKov1 <timur1711@iloud.com>
2022-11-16 16:02:23 +0900

    Удаление папки dist

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const StatoscopePlugin = require('@statoscope/webpack-plugin').default;
 const LodashWebpackPlugin = require('lodash-webpack-plugin')
-const RemovePlugin = require('remove-files-webpack-plugin');
 
 const config = {
     entry: {
@@ -29,13 +28,6 @@ const config = {
             collections: true,
             paths: true
         }),
-        new RemovePlugin({
-            after: {
-                include: [
-                    './dist'
-                ]
-            }
-        })
     ],
     output: {
         path: path.resolve(__dirname, 'dist'),
